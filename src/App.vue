2426b0f1e0eafdce4287f32f172f29344b02a053
Sara Rooijakkers <sara.rooijakkers@gmail.com>
2022-01-27 15:54:12 +0100

    Replace hexagons with variable shapes.

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,35 @@
     <div class="wrapper">
         <canvas ref="canvas" />
         <div class="buttons">
-            <button @click="setCompositeOperation('source-over')">source-over</button>
-            <button @click="setCompositeOperation('source-in')">source-in</button>
-            <button @click="setCompositeOperation('source-out')">source-out</button>
-            <button @click="setCompositeOperation('source-atop')">source-atop</button>
-            <button @click="setCompositeOperation('destination-over')">destination-over</button>
-            <button @click="setCompositeOperation('destination-in')">destination-in</button>
-            <button @click="setCompositeOperation('destination-out')">destination-out</button>
-            <button @click="setCompositeOperation('destination-atop')">destination-atop</button>
-            <button @click="setCompositeOperation('lighter')">lighter</button>
-            <button @click="setCompositeOperation('copy')">copy</button>
-            <button @click="setCompositeOperation('xor')">xor</button>
-            <button @click="setCompositeOperation('multiply')">multiply</button>
-            <button @click="setCompositeOperation('screen')">screen</button>
-            <button @click="setCompositeOperation('overlay')">overlay</button>
-            <button @click="setCompositeOperation('darken')">darken</button>
-            <button @click="setCompositeOperation('lighten')">lighten</button>
-            <button @click="setCompositeOperation('color-dodge')">color-dodge</button>
-            <button @click="setCompositeOperation('color-burn')">color-burn</button>
-            <button @click="setCompositeOperation('hard-light')">hard-light</button>
-            <button @click="setCompositeOperation('soft-light')">soft-light</button>
-            <button @click="setCompositeOperation('difference')">difference</button>
-            <button @click="setCompositeOperation('exclusion')">exclusion</button>
-            <button @click="setCompositeOperation('hue')">hue</button>
-            <button @click="setCompositeOperation('saturation')">saturation</button>
-            <button @click="setCompositeOperation('color')">color</button>
-            <button @click="setCompositeOperation('luminosity')">luminosity</button>
+            <button @click="setCompositeOperation('source-over'); render()">source-over</button>
+            <button @click="setCompositeOperation('source-in'); render()">source-in</button>
+            <button @click="setCompositeOperation('source-out'); render()">source-out</button>
+            <button @click="setCompositeOperation('source-atop'); render()">source-atop</button>
+            <button @click="setCompositeOperation('destination-over'); render()">destination-over</button>
+            <button @click="setCompositeOperation('destination-in'); render()">destination-in</button>
+            <button @click="setCompositeOperation('destination-out'); render()">destination-out</button>
+            <button @click="setCompositeOperation('destination-atop'); render()">destination-atop</button>
+            <button @click="setCompositeOperation('lighter'); render()">lighter</button>
+            <button @click="setCompositeOperation('copy'); render()">copy</button>
+            <button @click="setCompositeOperation('xor'); render()">xor</button>
+            <button @click="setCompositeOperation('multiply'); render()">multiply</button>
+            <button @click="setCompositeOperation('screen'); render()">screen</button>
+            <button @click="setCompositeOperation('overlay'); render()">overlay</button>
+            <button @click="setCompositeOperation('darken'); render()">darken</button>
+            <button @click="setCompositeOperation('lighten'); render()">lighten</button>
+            <button @click="setCompositeOperation('color-dodge'); render()">color-dodge</button>
+            <button @click="setCompositeOperation('color-burn'); render()">color-burn</button>
+            <button @click="setCompositeOperation('hard-light'); render()">hard-light</button>
+            <button @click="setCompositeOperation('soft-light'); render()">soft-light</button>
+            <button @click="setCompositeOperation('difference'); render()">difference</button>
+            <button @click="setCompositeOperation('exclusion'); render()">exclusion</button>
+            <button @click="setCompositeOperation('hue'); render()">hue</button>
+            <button @click="setCompositeOperation('saturation'); render()">saturation</button>
+            <button @click="setCompositeOperation('color'); render()">color</button>
+            <button @click="setCompositeOperation('luminosity'); render()">luminosity</button>
 
-            <button @click="moreX()">More X</button>
-            <button @click="lessX()">Less X</button>
-            <button @click="moreY()">More Y</button>
-            <button @click="lessY()">Less Y</button>
+            <button @click="more(); render()">More</button>
+            <button @click="less(); render()">Less</button>
         </div>
     </div>
 </template>
@@ -40,6 +38,14 @@
 <script setup lang="ts">
 import { ref } from '@vue/reactivity';
 import { onMounted } from '@vue/runtime-core';
+
+const incrementalX = 120;
+const incrementalY = 160;
+const clusterSize = 50;
+const shapeSize = 50;
+const shapeSideCount = ref<number>(3);
+const more = () => shapeSideCount.value++;
+const less = () => shapeSideCount.value--;
 
 const canvas = ref<HTMLCanvasElement | null>(null);
 const getContext = (): CanvasRenderingContext2D | null => {
@@ -49,7 +55,6 @@ const setCompositeOperation = (operation: string) => {
     const context = getContext();
     if (context) {
         context.globalCompositeOperation = operation;
-        renderFuckingEverything();
     }
 }
 const clearCanvas = () => {
@@ -59,65 +64,69 @@ const clearCanvas = () => {
     }
 }
 
-const incrementalX = ref<number>(120);
-const incrementalY = ref<number>(160);
-const moreX = () => {
-    incrementalX.value += 20;
-    renderFuckingEverything();
-}
-const lessX = () => {
-    incrementalX.value -= 20;
-    renderFuckingEverything();
-}
-const moreY = () => {
-    incrementalY.value += 20;
-    renderFuckingEverything();
-}
-const lessY = () => {
-    incrementalY.value -= 20;
-    renderFuckingEverything();
-}
+/**
+ * Generator that keeps cycling through the hex codes for red, blue, green.
+ * @generator
+ * @yields {string} Hex code.
+ */
+function* colors(): Generator<string> {
+    let availableColors: string[] = [
+        "#FF0000",
+        "#00FF00",
+        "#0000FF",
+    ];
 
-const drawHexagons = (xStart: number, yStart: number, colors: string[]) => {
-    const size = 50;
-    let x: number = xStart;
-    let y: number = yStart;
+    while (availableColors.length > 0) {
+        yield availableColors[0];
+        availableColors.push(availableColors[0]);
+        availableColors.shift();
+    }
+}
+const colorIterator = colors();
 
+/**
+ * Draw a cluster of shapes at the given coordinates.
+ * @param {number} x
+ * @param {number} y
+ */
+const drawShapeCluster = (x: number, y: number) => {
     for (let side = 0; side <= 6; side += 1) {
-        x = x + size * Math.cos(side * 2 * Math.PI / 6);
-        y = y + size * Math.sin(side * 2 * Math.PI / 6);
-        drawHexagon(x, y, colors[side % colors.length]);
+        drawShape(
+            x + clusterSize * Math.cos(side * 2 * Math.PI / 6),
+            y + clusterSize * Math.sin(side * 2 * Math.PI / 6)
+        );
     }
 }
 
-const drawHexagon = (x: number, y: number, color: string) => {
-    const size = 50;
+/**
+ * Draw a single shape at the given coordinates.
+ * @param {number} x
+ * @param {number} y
+ */
+const drawShape = (x: number, y: number) => {
     const context = getContext();
 
     if (context) {
         context.beginPath();
-        context.moveTo(x + size * Math.cos(0), y + size * Math.sin(0));
+        context.moveTo(x + shapeSize * Math.cos(0), y + shapeSize * Math.sin(0));
 
-        for (let side = 1; side <= 6; side += 1) {
-            context.lineTo(x + size * Math.cos(side * 2 * Math.PI / 6), y + size * Math.sin(side * 2 * Math.PI / 6));
+        for (let side = 1; side <= shapeSideCount.value; side += 1) {
+            context.lineTo(
+                x + shapeSize * Math.cos(side * 2 * Math.PI / shapeSideCount.value),
+                y + shapeSize * Math.sin(side * 2 * Math.PI / shapeSideCount.value)
+            );
         }
 
-        context.fillStyle = color;
+        context.fillStyle = colorIterator.next().value;
         context.fill();
     }
 }
 
-const renderFuckingEverything = () => {
+const render = () => {
     clearCanvas();
     const context = getContext();
     if (context) {
         context.globalAlpha = .75;
-
-        let colors = [
-            "#FF0000",
-            "#00FF00",
-            "#0000FF",
-        ];
 
         const initialX = -100;
         const initialY = -100;
@@ -126,12 +135,10 @@ const renderFuckingEverything = () => {
 
         while (y < window.innerHeight - initialY) {
             while (x < window.innerWidth - initialX) {
-                drawHexagons(x, y, colors);
-                colors.push(colors[0]);
-                colors.shift();
-                x += incrementalX.value;
+                drawShapeCluster(x, y);
+                x += incrementalX;
             }
-            y += incrementalY.value;
+            y += incrementalY;
             x = initialX;
         }
     }
@@ -142,13 +149,9 @@ onMounted(() => {
         canvas.value.width = window.innerWidth;
         canvas.value.height = window.innerHeight;
 
-        const context = getContext();
-        if (context) {
-            context.globalCompositeOperation = "lighter";
-        }
+        setCompositeOperation("lighter");
+        render();
     }
-
-    renderFuckingEverything();
 });
 </script>
 
